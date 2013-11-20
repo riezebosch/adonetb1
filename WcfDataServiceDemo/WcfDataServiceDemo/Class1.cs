43f217ba15dcc19152e854cace03d97434178fd4
Manuel Riezebosch <manuel.riezebosch@infosupport.com>
2013-11-20 22:48:14 +0100

    WCF Data Services

---
+++ WcfDataServiceDemo/WcfDataServiceDemo/Class1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WcfDataServiceDemo
+{
+    public class Persoon
+    {
+        public Persoon()
+        {
+            Producten = new List<Product>();
+        }
+
+        public int Id { get; set; }
+        public string Naam { get; set; }
+        public DateTime Geboortedatum { get; set; }
+
+        [MaxLength(50)]
+        [Required]
+        public string Email { get; set; }
+
+        public virtual ICollection<Product> Producten { get; set; }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [MaxLength(500)]
+        public string Omschrijving2 { get; set; }
+        public decimal Prijs { get; set; }
+
+        public virtual ICollection<Persoon> Klanten { get; set; }
+    }
+
+    public class MijnContext : DbContext
+    {
+        static MijnContext()
+        {
+            Database.SetInitializer<MijnContext>(new MijnInitializer());
+        }
+
+        public MijnContext()
+        {
+            Configuration.ProxyCreationEnabled =
+                Configuration.LazyLoadingEnabled = false;
+        }
+
+        public DbSet<Persoon> Personen { get; set; }
+        public DbSet<Product> Producten { get; set; }
+
+        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+        //}
+    }
+
+    class MijnInitializer : DropCreateDatabaseAlways<MijnContext>
+    {
+        protected override void Seed(MijnContext context)
+        {
+            base.Seed(context);
+            context.Personen.Add(new Persoon
+            {
+                Naam = "Manuel",
+                Geboortedatum = new DateTime(1982, 5, 4),
+                Email = "mriezebosch@gmail.com",
+                Producten = new List<Product>
+                {
+                    new Product { Omschrijving2 = "Printer", Prijs = 199m },
+                    new Product { Omschrijving2 = "Muis", Prijs = 19.99m }
+                }
+            });
+        }
+    }
+}
---
+++ WcfDataServiceDemo/WcfDataServiceDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WcfDataServiceDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WcfDataServiceDemo")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9cc051ad-57e7-4b7a-bdca-9f235a10c9b7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
